7f415cfd567458c5354547b924e1d4c4ce83404a
Guillermo Vaya <guivaya@gmail.com>
2022-07-16 20:22:05 +0200

    move to lhs

--- backlinks.ts
+++ backlinks.ts
@@ -1,27 +1,39 @@
 
-import {hideRhs, showRhs} from "@silverbulletmd/plugos-silverbullet-syscall/editor";
+import {hideLhs, showLhs} from "@silverbulletmd/plugos-silverbullet-syscall/editor";
 import * as clientStore from "@silverbulletmd/plugos-silverbullet-syscall/clientStore"
+import {css} from "./css";
 
 const BackLinksKey = "showBacklinks"
 
 export async function toggleBacklinks() {
-  let showingBacklinks = !!(await clientStore.get(BackLinksKey));
+  const showingBacklinks = (await getBacklinkStatus());
   await clientStore.set(BackLinksKey, !showingBacklinks);
   if (!showingBacklinks) {
-    await showRhs(
-      `<html><head></head><body>some backlinks</body></html>`,
-      undefined,
-      2
-    );
+    await showBacklinks("<li>some backlinks</li>");
   } else {
-    await hideRhs()
+    await hideLhs()
   }
 }
 
 export async function updateBacklinks() {
-  await showRhs(
-    `<html><head></head><body>more backlinks</body></html>`,
-    undefined,
-    2
-  );
+  await showBacklinks("<li>more backlinks</li>");
+}
+
+async function showBacklinks(content) {
+  if (await getBacklinkStatus()) {
+    await showLhs(
+      `<html><head>${css}</head><body>
+        <h2>Backlinks to this page</h2>
+        <ul>
+        ${content}
+        </ul>
+        </body></html>`,
+      undefined,
+      1
+    );
+  }
+}
+
+async function getBacklinkStatus() {
+  return !!(await clientStore.get(BackLinksKey));
 }
